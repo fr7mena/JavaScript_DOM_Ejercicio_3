7efb312550de7b76f2de676ff2e0d4f97cf983c9
fr7mena <fr7mena@gmail.com>
2025-01-22 16:47:41 +0100

    Realizado correctamente todo el ejercicio

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let hermano;
+
 document.addEventListener('DOMContentLoaded', event => {
     //Cogemos todos los elementos de tipo H2
     let headers = document.getElementsByTagName('h2');
@@ -17,14 +19,36 @@ function recorrerAdelante(e) {
     let texto = "Has elegido " + textoDelH2 +  " que está situada en el " + divPadreId;
 
     texto = recorrerHermanos(header, texto);
-
-    console.log(texto);
+    texto = recorrerHijos(header, texto);
+    /*console.log(texto);*/
+    let parrafo = document.createElement("p");
+    parrafo.textContent = texto;
+    document.body.appendChild(parrafo);
 }
 
 function recorrerHermanos(header, texto) {
-    const hermano = header.nextElementSibling; //Con este metodo consigo obtener el hermano, es decir un elemento que esté en la misma jerarquía o nivel desde el elemento desde el que se llama al metodo, en este caso es un <ul>
+    hermano = header.nextElementSibling; //Con este metodo consigo obtener el hermano, es decir un elemento que esté en la misma jerarquía o nivel desde el elemento desde el que se llama al metodo, en este caso es un <ul>
     const numeroProvinvias = hermano.children.length; //Con esto obtengo el numero total de <li> que tiene este ul, aunque realmente este metodo (realmente no se si es un metodo o una propiedad del objeto tipo elemento) ".children.length" te saca el número de elementos hijos que cuelgan de ese elemento padre desde el que se llama.
 
     texto += ", y esta CCAA tiene un total de " + numeroProvinvias + " provincias.";
     return texto;
 }
+
+function recorrerHijos(header, texto) {
+    const hermanoHijos = hermano.children;
+    let contador = 0;
+    let textoAdiccion = " Las cuáles son:";
+    for (const hijo of hermanoHijos) {
+        if (contador == 0) {
+            textoAdiccion += " " + hijo.textContent;
+        } else {
+            textoAdiccion += ", " + hijo.textContent;
+        }
+        contador++;
+    }
+    texto += textoAdiccion + ".\n";
+    return texto;
+}
+
+
+
